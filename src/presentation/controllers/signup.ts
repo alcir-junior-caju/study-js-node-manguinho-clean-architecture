9e0110d9aaa601b1c204062b70b73ff59c8c6f04
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-24 22:08:07 -0300

    feat: ensure SignUpController return an error if an invalid email is provided and exports index

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,14 @@
-import { HttpResponse, HttpRequest } from '../protocols/http';
-import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper';
-import { Controller } from '../protocols/controller';
+import { HttpResponse, HttpRequest, Controller, EmailValidator } from '../protocols';
+import { InvalidParamError, MissingParamError } from '../errors'
+import { badRequest } from '../helpers';
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -11,5 +16,8 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field])
         return badRequest(new MissingParamError(field))
     }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) return badRequest(new InvalidParamError('email'))
   }
 }
