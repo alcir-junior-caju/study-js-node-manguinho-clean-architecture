f3fa4e43b0e85526b2fbd850afd6f2b0d85b2743
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-03-27 20:01:24 -0300

    chore: Change config jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
+  coverageReporters: ["text-summary", "text", "lcov"],
+  verbose: true,
 };
