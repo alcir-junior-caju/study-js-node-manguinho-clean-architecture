dad17799e29a3adbec22c584dffbb37ea1bf65f6
Alcir Junior <junior@cajucomunica.com.br>
2021-05-20 17:34:32 -0300

    feat: ensure DbAuthentication calls TokenGenerator witj correct id

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,5 +1,6 @@
 export * from './criptography/encrypter'
 export * from './criptography/hash-comparer'
+export * from './criptography/token-generator'
 export * from './db/add-accountrepository'
 export * from './db/load-account-by-email-repository'
 export * from './db/log-error-repository'
